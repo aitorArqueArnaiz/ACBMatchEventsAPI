0fd094d3292b63c4cb46cdfbca54b2d2010b437b
aitoredualdnun <aarque@userzoom.com>
2024-05-26 15:57:26 +0200

    Added summaries to in memmory repository

--- MatchEvents.Domain/Interfaces/IInMemmoryRepository.cs
+++ MatchEvents.Domain/Interfaces/IInMemmoryRepository.cs
@@ -4,7 +4,19 @@ namespace MatchEvents.Domain.Interfaces
 {
     public interface IInMemmoryRepository
     {
+        /// <summary>
+        /// gets the match event given a gameId.
+        /// </summary>
+        /// <param name="id">The game identifier.</param>
+        /// <returns>The match event data.</returns>
         Task<IEnumerable<MatchEventInfo>> GetMatchEventsAsync(int id);
+
+        /// <summary>
+        /// Creates a new match event with key as the game identifier.
+        /// </summary>
+        /// <param name="id">The game identifier.</param>
+        /// <param name="matchEvent">The match event information.</param>
+        /// <returns></returns>
         Task CreateMatchEventsAsync(int id, IEnumerable<MatchEventInfo> matchEvent);
     }
 }
